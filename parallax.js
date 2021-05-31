831cf915471cc073fea1d0f5690d42adf914faba
Bruno Cicerchia <brunocicerchia.dev@gmail.com>
2021-05-31 20:50:43 -0300

    Ashe

---
+++ parallax.js
@@ -0,0 +1,9 @@
+//let parallax = document.querySelector('.parallax');
+
+//function scrollParallax() {
+//    let ScrollTop = document.documentElement.scrollTop;
+//    parallax.style.transform = 'translateY(' + ScrollTop * -0.3 + 'px)';
+//    console.log("moviendo xd")
+//}
+
+//window.addEventListener('scroll', scrollParallax);
